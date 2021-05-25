73d274d573a046de65e5783c45023591cb30e74b
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-05-25 22:28:34 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express(); //now express app is ready to used
+
+// to handle request we send response
+
+app.get("/", (req, res) => {
+  res.send("Hello ! this is my social media app!!");
+});
+
+//server will listen to this port
+app.listen(8080, () => {
+  console.log("server connected");
+});
