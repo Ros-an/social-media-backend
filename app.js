c46e9ff292597a7288a25fa7188066adeba19600
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-05-26 15:08:03 +0530

    mongoose db connection

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
 const app = express();
 dotenv.config();
 
+// DB connection
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB CONNECTION SUCCESSFUL!"));
+mongoose.connection.on("error", (err) => {
+  console.log(`DB CONNECTION ERROR: ${err.message}`);
+});
 // bring in routes
 const postRoutes = require("./routes/post.route");
 
 // middleware
 app.use(morgan("dev"));
-
 app.use(postRoutes);
 
 const port = process.env.PORT || 8080;
