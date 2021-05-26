fc7994dcb7bf17ec52937f174b63358b6f9571e3
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-05-26 06:45:52 +0530

    separating routes

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 const express = require("express");
-const app = express(); //now express app is ready to used
+const app = express();
 
-// to handle request we send response
+// bring in routes
+const postRoute = require("./routes/post");
+app.get("/", postRoute.getPost);
 
-app.get("/", (req, res) => {
-  res.send("Hello ! this is my social media app with nodemon!!");
-});
-
-//server will listen to this port
 app.listen(8080, () => {
   console.log("server connected");
 });
---
+++ routes/post.js
@@ -0,0 +1,7 @@
+const getPost = (req, res) => {
+  res.send("Hello ! this is my social media app with nodemon!!");
+};
+
+module.exports = {
+  getPost,
+};
