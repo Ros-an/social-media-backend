6a837d286cae612b8262e396870f53a6129b455e
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-05-31 15:19:24 +0530

    post schema update

--- models/post.model.js
+++ models/post.model.js
@@ -1,18 +1,29 @@
 const mongoose = require("mongoose");
-
-const postSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: "Title is required",
-    minlength: 4,
-    maxlength: 150,
+const { ObjectId } = mongoose.Schema;
+const postSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: "Title is required",
+      minlength: 4,
+      maxlength: 150,
+    },
+    body: {
+      type: String,
+      required: "Body is required",
+      minlength: 4,
+      maxlength: 1500,
+    },
+    photo: {
+      data: Buffer,
+      contentType: String,
+    },
+    postedBy: {
+      type: ObjectId,
+      ref: "User", //referring to model User
+    },
   },
-  body: {
-    type: String,
-    required: "Body is required",
-    minlength: 4,
-    maxlength: 1500,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Post", postSchema);
