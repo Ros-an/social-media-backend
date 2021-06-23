986595d1ec6214ee53e07161ecaefdb6f3ce2df1
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-06-23 06:13:52 +0530

    removed console.log

--- error-handler/index.js
+++ error-handler/index.js
@@ -1,5 +1,4 @@
 function errorHandler(err, req, res, next) {
-  console.log(err.stack);
   res.status(500).json({
     success: false,
     message: "Error occurred, check the error message for more details",
