01faee4e5c32e2ccbde270f0ee9aebfcd44b3177
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-06-23 06:17:13 +0530

    refactor: removed title field

--- models/post.model.js
+++ models/post.model.js
@@ -2,19 +2,18 @@ const mongoose = require("mongoose");
 const { ObjectId } = mongoose.Schema;
 const postSchema = new mongoose.Schema(
   {
-    title: {
+    // title: {
+    //   type: String,
+    //   required: "Title is required",
+    //   minlength: 4,
+    //   maxlength: 150,
+    // },
+    post: {
       type: String,
-      required: "Title is required",
-      minlength: 4,
-      maxlength: 150,
-    },
-    body: {
-      type: String,
-      required: "Body is required",
-      minlength: 4,
+      required: "post body is required",
       maxlength: 1500,
     },
-    photo: {
+    postphoto: {
       data: Buffer,
       contentType: String,
     },
